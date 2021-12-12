14d634a184f41d914511cb04af47476d0160bea5
arvindkalbhor@gmail.com <arvindkalbhor@gmail.com>
2021-12-12 11:41:52 +0530

    git configs and npm dependencies

--- modals/usermodel.js
+++ modals/usermodel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name:{
     type: String,
-    required: [true, 'A user must have a name'],
+    required: [true, 'A user must have a name '],
     unique: true,
     trim: true
   }
