71ece865a2c0fe0b52a6c61d3e7b1b6fd87b8400
arvind kalbhor <arvindkalbhor@gmail.com>
2021-12-13 09:18:35 +0530

    new changes in matchmodel for creating match

--- models/matchmodel.js
+++ models/matchmodel.js
@@ -1,29 +1,70 @@
 const mongoose = require('mongoose');
 
 const matchSchema = new mongoose.Schema({
-  name:{
+  tName:{
     type: String,
-    required: [true, ''],
+    required: [true, 'a match must have a name'],
     unique: true,
     trim: true
   }, 
-  description:{
-    type: String,
-    trim: true
-  },
-  imageCover: {
-    type: String,
-    required: [false, '']
-  },
-  images: [String],
-  createdAt: {
+  tStartDate:{
     type: Date,
     default: Date.now()
   },
-  startDates: [Date]
+  tEndDate:{
+    type: Date
+  },
+  tBanner: String,
+  tPrizes: [],
+  tContacts: [],
+  payment:{},
+  matches: [],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
 });
 
 const Match = mongoose.model('Match', matchSchema);
+// const data = {
+//   tName:"",
+//   tStartDate:"",
+//   tEndDate:"",
+//   tBanner:"234234234.png",
+//   tPrizes:[{
+//     prizeNumber:1,
+//     prizeMoney:"10000",
+//     sponsor:"",
+//   },
+//   {
+//     prizeNumber:2,
+//     prizeMoney:"7000",
+//     sponsor:"",
+//   },
+//   {
+//     prizeNumber:3,
+//     prizeMoney:"5000",
+//     sponsor:"",
+//   }],
+//   tContacts:[{
+//     name:"",
+//     mob:""
+//   },{
+//     name:"",
+//     mob:""
+//   }],
+//   payment:{},
+//   matches:{
+//     tWinners:[
+//       {winnerNumber:1,cityName:"kashil"},
+//       {winnerNumber:2,cityName:"satara"},
+//       {winnerNumber:3,cityName:"dsfsdfsdf"}
+//     ],
+//     halfWinner:[ {day:1,date:"",cityName:"kashil"},
+//     {day:2,date:"",cityName:"kashil"}]
+//   }
+
+// }
 
 
 module.exports = Match;
