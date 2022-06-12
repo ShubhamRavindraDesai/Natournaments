22e364ac2b9c016acdf2843a09b75f8d017e8b38
Shubham desai <desai99150@gmail.com>
2022-06-12 12:54:18 +0530

    test

--- app.js
+++ app.js
@@ -76,9 +76,9 @@ app.get('/', (req, res)=>{
 app.use('/api/v1/matches', matchRouter);
 app.use('/api/v1/users', userRouter);
 
-app.all('*', (req, res, next) => {
-  next(new AppError(`can't find ${req.originalUrl} on this server!`, 404));
-});
+// app.all('*', (req, res, next) => {
+//   next(new AppError(`can't find ${req.originalUrl} on this server!`, 404));
+// });
 
 app.use(globalErrorHandler);
 module.exports = app;
